274aa8b9a9e14f5d0c5a2be348d4785d69f70a7c
Daniil Kuzjomin <93324329+DaniilKuzjomin@users.noreply.github.com>
2022-02-01 14:04:39 +0200

    Update quadur.py

--- quadur/quadur.py
+++ quadur/quadur.py
@@ -53,51 +53,45 @@ def solution():
     return graf,D,t
 
 
-def fish():
-    x1 = np.arange(0, 9.5, 0.5)#min max step
-    y1=(2/27)*x1*x1-3
-    x2 = np.arange(-10, 0.5, 0.5)#min max step
-    y2=0.04*x2*x2-3
-    x3 = np.arange(-9, -2.5, 0.5)#min max step
-    y3=(2/9)*(x3+6)**2+1
-    x4 = np.arange(-3, 9.5, 0.5)#min max step
-    y4=(-1/12)*(x4-3)**2+6
-    x5 = np.arange(5, 9, 0.5)#min max step
-    y5=(1/9)*(x5-5)**2+2
-    x6 = np.arange(5, 8.5, 0.5)#min max step
-    y6=(1/8)*(x6-7)**2+1.5
-    x7 = np.arange(-13, -8.5, 0.5)#min max step
-    y7=(-0.75)*(x7+11)**2+6
-    x8 = np.arange(-15, -12.5, 0.5)#min max step
-    y8=(-0.5)*(x8+13)**2+3
-    x9 = np.arange(-15, -10, 0.5)#min max step
-    y9=[1]*len(x9)
-    x10 = np.arange(3, 4, 0.5)#min max step
-    y10=[3]*len(x10)
-    fig = plt.figure()
-    plt.plot(x1, y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7,x8,y8,x9,y9,x10,y10)
-    plt.title('Кит')
+def bird():
+    x1 = np.arange(0, 9.5)
+    y1 =(-4/27)*x1**2+6
+    x2 = np.arange(-2, 7.5)
+    y2 = (1/9)*(x2-7)**2-4
+    x3 = np.arange(-4, 0.5)
+    y3 = (-0.5)*(x3+2)**2+8
+    x4 = np.arange(-6, -1.5)
+    y4 = (-1/16)*(x4+2)**2+5
+    x5 = np.arange(-6, -3.5)
+    y5 = x5+10
+    x6 = np.arange(7, 9.5)
+    y6 = (-x6)+3
+    x7 = np.arange(-6, 1.5)
+    y7 = 0.5*x7-1
+    x8 = np.arange(-5,2.5)
+    y8 = 0.5*x8-2.5
+    plt.plot(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7,x8,y8)
+    plt.title('Птица')
     plt.ylabel('y')
     plt.xlabel('x')
     plt.grid(True)
     plt.show()
 
-
 def glasses():
     x1= np.arange(-9, -0.5, 0.5)
-    y1=-1*(1/16)*(x1-5)**2+2
+    y1=-(1/16)*(x1+5)**2+2
     x2= np.arange(1,9.5, 0.5)
-    y2=-1*(1/16)*(x2-5)**2+2
+    y2=-(1/16)*(x2-5)**2+2
     x3= np.arange(-9,-0.5,0.5)
     y3=(1/4)*(x3+5)**2-3
     x4= np.arange(1,9.5,0.5)
-    y4=(1/4)*(x4+5)**2-3
+    y4=(1/4)*(x4-5)**2-3
     x5= np.arange(-9,-5.5,0.5)
-    y5=-1*(x5+7)**2+5
+    y5=-(x5+7)**2+5
     x6= np.arange(6,9.5, 0.5)
-    y6=-1*(x6+7)**2+5
+    y6=-(x6-7)**2+5
     x7= np.arange(-1,1.5,0.5)
-    y7=-1*0.5*x7**2+1.5
+    y7=-(0.5)*x7**2+1.5
     plt.plot(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7)
     plt.title('Очки')
     plt.ylabel('y')
@@ -105,17 +99,34 @@ def glasses():
     plt.grid(True)
     plt.show()
 
-def konn():
-    pass
+def umbr():
+    x1 = np.arange(-12, 12.5, 0.5)
+    y1 = (-1/18)*x1**2+12
+    x2 = np.arange(-4, 4.5, 0.5)
+    y2 = (-1/8)*x2**2+6
+    x3 = np.arange(-12, -3.5, 0.5)
+    y3 = (-1/8)*(x3+8)**2+6
+    x4 = np.arange(4, 12.5, 0.5)
+    y4 = (-1/8)*(x4-8)**2+6
+    x5 = np.arange (-4, 0.2, 0.5)
+    y5 = 2*(x5+3)**2-9
+    x6 = np.arange(-4, 0.7, 0.5)
+    y6 = 1.5*(x6+3)**2-10
+    plt.plot(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6)
+    plt.title('Зонтик')
+    plt.ylabel('y')
+    plt.xlabel('x')
+    plt.grid(True)
+    plt.show()
 
 def figure():
     choice=var.get()
     if choice==1:
-        fish()
+        bird()
     if choice==2:
         glasses()
     else:
-        konn()
+        umbr()
 
 
 
@@ -156,9 +167,9 @@ def plus():
 
 window=Tk()
 window.title("Квадратные уравнения")
-window.geometry("650x300")
+window.geometry("650x210")
 
-fr1=Frame(window,width=650, height=200)
+fr1=Frame(window,width=650, height=210)
 fr1.pack(side=TOP)
 fr2=Frame(window,width=650, height=200)
 fr2.pack(side=BOTTOM)
@@ -197,9 +208,9 @@ l4.pack(side=BOTTOM,padx=3, pady=0)
 bt_plus=Button(fr2,text="Увеличить окно", font="Calibri 26" ,bg="green",command=plus)
 bt_plus.pack(side = TOP)
 var=IntVar()
-r1=Radiobutton(fr2,text="Кит",variable=var,value=1, font="Calibri 26", command=figure)
+r1=Radiobutton(fr2,text="Птичка",variable=var,value=1, font="Calibri 26", command=figure)
 r2=Radiobutton(fr2,text="Очки",variable=var,value=2, font="Calibri 26", command=figure)
-r3=Radiobutton(fr2,text="Лягуха",variable=var,value=3, font="Calibri 26",command=figure)
+r3=Radiobutton(fr2,text="Зонтик",variable=var,value=3, font="Calibri 26",command=figure)
 r1.pack()
 r2.pack()
 r3.pack()
